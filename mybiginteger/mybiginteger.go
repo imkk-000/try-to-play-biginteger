ecc29476a5e9f46713930130a45063b4478ba115
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-21 13:00:44 +0700

    [Added]: function: Add for sum two biginteger (support only integer not support precision and sign number)

--- mybiginteger/mybiginteger.go
+++ mybiginteger/mybiginteger.go
@@ -4,17 +4,33 @@ import (
 	"unicode"
 )
 
-func isAllowedRune(r rune) bool {
-	return unicode.IsDigit(r) || (r == '.') || (r == '-')
+func isAllowedRune(index int, r rune) bool {
+	return unicode.IsDigit(r) || (index == 0 && r == '-')
 }
 
 func ConvertStringToBytes(numberString string) []byte {
-	result := []byte{}
-	for _, digit := range []rune(numberString) {
-		if !isAllowedRune(digit) {
+	var result []byte
+	for index, digit := range []rune(numberString) {
+		if !isAllowedRune(index, digit) {
 			return nil
 		}
 		result = append(result, (byte(digit) - '0'))
 	}
 	return result
+}
+
+// Add - Accumulator = Number1 + Number2
+func Add(number1 []byte, number2 []byte) []byte {
+	number1Length := len(number1)
+	accumulatorBytes := make([]byte, number1Length+1)
+	var accumulatorPerDigit byte
+	for index := number1Length; index > 0; index-- {
+		accumulatorPerDigit = (number1[index-1] + number2[index-1])
+		accumulatorBytes[index] += accumulatorPerDigit % 10
+		accumulatorBytes[index-1] = accumulatorPerDigit / 10
+	}
+	if accumulatorBytes[0] == 0 {
+		return accumulatorBytes[1:]
+	}
+	return accumulatorBytes
 }
--- mybiginteger/mybiginteger_test.go
+++ mybiginteger/mybiginteger_test.go
@@ -14,17 +14,31 @@ func TestConvertStringToBytes(t *testing.T) {
 	testData := []TestData{
 		{[]byte{1, 2, 3, 5}, "1235"},
 		{[]byte{253, 1, 2, 3, 5}, "-1235"},
-		{[]byte{1, 2, 3, 254, 5}, "123.5"},
-		{[]byte{253, 1, 2, 3, 254, 5}, "-123.5"},
 		{[]byte(nil), "a1235"},
 		{[]byte(nil), "12$35"},
 		{[]byte(nil), "1235@"},
 	}
-
 	for _, testCase := range testData {
 		actualBytes := ConvertStringToBytes(testCase.inputData)
 		if !reflect.DeepEqual(testCase.expectedResult, actualBytes) {
 			t.Errorf("expect %v but it got %v", testCase.expectedResult, actualBytes)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	type TestData struct {
+		expectedResult   []byte
+		inputDataNumber1 []byte
+		inputDataNumber2 []byte
+	}
+	testData := []TestData{
+		{[]byte{3, 3}, []byte{2, 2}, []byte{1, 1}},
+	}
+	for _, testCase := range testData {
+		actualAddBytes := Add(testCase.inputDataNumber1, testCase.inputDataNumber2)
+		if !reflect.DeepEqual(testCase.expectedResult, actualAddBytes) {
+			t.Errorf("expect %v but it got %v", testCase.expectedResult, actualAddBytes)
 		}
 	}
 }
